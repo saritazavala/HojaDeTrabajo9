448dd9ba4a911fa34aefcb04408bb8ba9e972fad
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-05-17 17:53:20 -0600

    Mas clases

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Me gustas Gerardito");
+        Scanner teclado = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Menu de opciones");
+            System.out.println("1. Buscar distancia entre ciudades");
+            System.out.println("2. Modificar Grafo");
+            System.out.println("3. Salir");
+
+
+
+
+            String opcion = teclado.nextLine();
+        }
     }
 }
