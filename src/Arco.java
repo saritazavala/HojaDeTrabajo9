260d45ab55167f0e0f25a9316ff4672f519f0b5c
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-05-17 18:09:13 -0600

    Creo que ya esta todo

--- src/Arco.java
+++ src/Arco.java
@@ -1,2 +1,46 @@
-public class Arco {
+import javax.xml.soap.Node;
+
+public class Arco<V,E> {
+
+    private Vertice origin;
+    private Vertice destination;
+    private double distance;
+
+    public Arco(Vertice origin, Vertice destination, double distance) {
+        this.origin = origin;
+        this.destination = destination;
+        this.distance = distance;
+    }
+
+    public Vertice getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Vertice origin) {
+        this.origin = origin;
+    }
+
+    public Vertice getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Vertice destination) {
+        this.destination = destination;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return "\n Edge [origin=" + origin.getCity() + ", destination=" + destination.getCity() + ", distance="
+                + distance + "]";
+    }
+
+
 }
--- src/Vertice.java
+++ src/Vertice.java
@@ -1,2 +1,43 @@
-public class Vertice {
+import com.sun.javafx.geom.Edge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertice<E> {
+
+    //https://devs4j.com/2017/11/24/implementa-un-grafo-de-ciudades-en-java/
+    private String city;
+    private List<Arco> edges;
+
+    public Vertice(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<Arco> getEdges() {
+        return edges;
+    }
+
+    public void addEdge(Arco edge) {
+        if (edges == null) {
+            edges = new ArrayList<>();
+        }
+        edges.add(edge);
+    }
+
+    @Override
+    public String toString() {
+        return "\n \tNode [city=" + city + ", edges=" + edges + "]";
+    }
+
+
+
+
 }
